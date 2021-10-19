ca4eb692b5201eae08f24038109b672787f8a86a
tryspidy <68438592+tryspidy@users.noreply.github.com>
2021-10-19 06:07:48 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+PrimaryKey:-
+
+A primary key constrain is a column or group of columns that uniquely identifies every row in the table of the relational database management system. 
+It cannot be a duplicate, meaning the same value should not appear more than once in the table.
+
+Foreign key:-
+A table can have more than one primary key. 
+Primary key can be defined at the column or the table level.
+If you create a composite primary key, it should be defined at the table level.
+Foreign key is a column that creates a relationship between two tables. The purpose of the Foreign key is to maintain data integrity and allow navigation between two different instances of an entity. It acts as a cross-reference between two tables as it references the primary key of another table. 
+Every relationship in the database should be supported by a foreign key.
